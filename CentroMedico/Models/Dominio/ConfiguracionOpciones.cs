fb4da5c93f0df0b632097bf5a29ee47295945364
Audeljassiel12 <134581299+Audeljassiel12@users.noreply.github.com>
2024-08-29 21:30:30 -0600

    ModficacionBasica

--- CentroMedico/Models/Dominio/ConfiguracionOpciones.cs
+++ CentroMedico/Models/Dominio/ConfiguracionOpciones.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public int CategoriaId { get; set; } // FK a ConfiguracionCategoria
         public string Clave { get; set; } // Ejemplo: "Peso", "Glucosa"
-        public string Descripcion { get; set; } // Ejemplo: "Peso", "Glucosa en ayunas"
         public bool EstaVisible { get; set; } // Determina si la opción está activa
 
         // Navegación a la categoría de configuración
