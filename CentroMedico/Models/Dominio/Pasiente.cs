6b7d6a3dc5f6c1c20c94c93533a65e76950f9855
Audeljassiel12 <134581299+Audeljassiel12@users.noreply.github.com>
2024-08-30 18:56:47 -0600

    pasiente

--- CentroMedico/Models/Dominio/Pasiente.cs
+++ CentroMedico/Models/Dominio/Pasiente.cs
@@ -5,27 +5,27 @@ namespace CentroMedico.Models.Dominio
     public class Paciente
     {
         public int Id { get; set; }
-        [Requerid(ErrorMensage = "Los {0} son requeridos para el siguiente paso ")]
+        [Required(ErrorMessage = "Los {0} son requeridos para el siguiente paso ")]
         [StringLength(maximumLength:16,MinimumLength =13,ErrorMessage ="Los {0} no cumple con los requisitos de longitud. asegúrese de que tenga entre  {1} y {2} caracteres.")]
         public string Nombres { get; set; } = null!;
-        [Requerid(ErrorMensage = "Los {0} son requeridos para seguir el siguiente paso")]
+        [Required(ErrorMessage = "Los {0} son requeridos para seguir el siguiente paso")]
         [StringLength(maximumLength: 30, MinimumLength = 40, ErrorMessage = "Los {0} no cumple con los requisitos de longitud. asegurese de que tenga entre  {1} y {2} caracteres. ")] 
         public string Apellidos { get; set; } = null!;
-        [Requerid(ErrorMensague = "La fecha de nacimiento es requerida para seguir el siguiente paso")] 
+        [Required(ErrorMessage = "La fecha de nacimiento es requerida para seguir el siguiente paso")] 
         [StringLength(maximumLength: 12, MinimumLength = 10, ErrorMessage = " Fecha invalida. Asegurese de usar el formato correcto dia, mes y año separados por /.")]
         [DataType(DataType.Date)]
         
         public DateTime FechaNacimiento { get; set; }
-        [Requerid(ErorMensage = "El {0} es requerido para el siguiente paso")]
+        [Required(ErrorMessage = "El {0} es requerido para el siguiente paso")]
         [StringLength(maximumLength: 1, MinimumLength = 1, ErrorMessage = "Valor no válido. Ingrese 'M' para Masculino, 'F' para Femenino. ")]
         public string Sexo { get; set; }  = null!;
-        public string Sexo { get; set; }  = null !;
-        [Requerid(ErrorMensage = "La direccion es requerida para el siguiente paso")]
+      
+        [Required(ErrorMessage = "La direccion es requerida para el siguiente paso")]
         [StringLength(maximumLength: 200, MinimumLength = 100, ErrorMessage = "La {0} es demasiado corta. Por favor, ingrese una dirección más detallada.")]
         public string Dirección { get; set; }
         [StringLength(maximumLength: 10, MinimumLength = 8, ErrorMessage = " El número de teléfono es demasiado largo. Por favor, use un máximo de {1} caracteres. ")]
         public string? Teléfono { get; set; }
-        [Requerid(ErrorMensage = "Es requerido su correo electronico para el siguiente paso")]
+        [Required(ErrorMessage = "Es requerido su correo electronico para el siguiente paso")]
         [StringLength(maximumLength: 70, MinimumLength = 20, ErrorMessage = " Formato de correo electrónico inválido. Asegúrese de incluir '@' y un dominio válido. ")]
         public string Email { get; set; } = null!;
         [StringLength(maximumLength: 3, MinimumLength = 2, ErrorMessage = " El tipo de sangre es demasiado largo. Por favor, use un máximo de {1} caracteres. ")]
